c833b7030a0d4872d5bf3bb3aef3677323306e26
Muhiman-001 <f219410@cfd.nu.edu.pk>
2023-10-06 09:52:15 +0500

    Created calculator class with add & subtract function only

---
+++ lab7/Calculator.java
@@ -0,0 +1,23 @@
+package lab7;
+
+public class Calculator {
+
+	public void add(int n1 , int n2)
+	{
+		int n3 =0;
+		
+		n3 = n1 + n2;
+		
+	    System.out.println(n3);
+	}
+
+	public void sub(int n1 , int n2)
+	{
+		int n3 =0;
+		
+		n3 = n1 - n2;
+		
+	    System.out.println(n3);
+	}
+	
+}
